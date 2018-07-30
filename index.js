e98599d8de370b27aa90a4b3e85ebf22795c3857
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-07-30 05:21:37 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  return "I can't hear you!";
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  return "YES INDEED!";
+  console.log(string.toUpperCase())
+}
