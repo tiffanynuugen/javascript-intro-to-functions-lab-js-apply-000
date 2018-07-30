e4a9679ad00db82fb20658ebbbb48dec305c87ed
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-07-30 06:10:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
     return "I can't hear you!";
-  } else if (string === string.toUpperCase()) {
+  } else (string === string.toUpperCase()) {
     return "YES INDEED!";
-  } else if (string === "I love you, Grandma.") {
+  } else (string === "I love you, Grandma.") {
     return "I love you, too.";
   }
 };
